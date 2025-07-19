e497c825265642c2fe301c628d5f8da9855fd3ba
reuben-idan <nobeadonis2@gmail.com>
2025-07-19 19:30:51 +0000

    fix: Add explicit 'from .models import Library' for checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Library
+from .models import Book
 
 # Function-based view to list all books (HTML template)
 def list_books(request):
