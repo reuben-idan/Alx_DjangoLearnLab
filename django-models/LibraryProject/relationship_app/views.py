7c71e6f280bd7ede570ef4972fc5e4ecc55ccb57
reuben-idan <nobeadonis2@gmail.com>
2025-07-19 19:18:13 +0000

    fix: Use Book.objects.all() and render plain text for function-based view as required by checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 
-# Function-based view to list all books
+# Function-based view to list all books (plain text)
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.all()
+    output = '\n'.join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type="text/plain")
 
 # Class-based view for library details
 class LibraryDetailView(DetailView):
