e4f26fc4733a8224c5878ec0af7ff2251529300b
reuben-idan <nobeadonis2@gmail.com>
2025-07-25 06:34:03 +0000

    update code snipet

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,9 +1,1 @@
-AUTH_USER_MODEL = 'bookshelf.CustomUser'
-
-DEBUG = False
-
-SECURE_BROWSER_XSS_FILTER = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_CONTENT_TYPE_NOSNIFF = True
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+AUTH_USER_MODEL = 'advanced_features_and_security.CustomUser'
