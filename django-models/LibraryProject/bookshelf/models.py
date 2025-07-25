fc9e3cb16c905a4f619e42ee98d72e7cc7e2d71b
reuben-idan <nobeadonis2@gmail.com>
2025-07-25 05:25:00 +0000

    update code snippet

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,11 +1,35 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, profile_photo, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    def create_superuser(self, email, date_of_birth, profile_photo, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, date_of_birth, profile_photo, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField(upload_to='profile_photos/')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth', 'profile_photo']
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.email
+
+class OtherModel(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    # other fields...
