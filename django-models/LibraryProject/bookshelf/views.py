61c6fe7036b717e6ab09e114c28ad167fed54a26
reuben-idan <nobeadonis2@gmail.com>
2025-07-25 05:47:22 +0000

    Implement and manage permissions and groups to control access to various parts of the  application, enhancing security and functionality.

--- django-models/LibraryProject/bookshelf/views.py
+++ django-models/LibraryProject/bookshelf/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        publication_date = request.POST['publication_date']
+        isbn = request.POST['isbn']
+        book = Book.objects.create(title=title, author=author, publication_date=publication_date, isbn=isbn)
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_create.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_update(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.publication_date = request.POST['publication_date']
+        book.isbn = request.POST['isbn']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_update.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = Book.objects.get(pk=pk)
+    book.delete()
+    return redirect('book_list')
