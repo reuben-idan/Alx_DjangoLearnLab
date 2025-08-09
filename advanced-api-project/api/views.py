4d4288b09771d5d060130fa9bc1827f70171823d
reuben-idan <nobeadonis2@gmail.com>
2025-08-09 19:00:32 +0000

    update views.py file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import ListView, DetailView, UpdateView, DeleteView, CreateView
